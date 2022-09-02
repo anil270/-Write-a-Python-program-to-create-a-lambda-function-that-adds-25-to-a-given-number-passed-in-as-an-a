a7531ddc056ec099ddb898bc6a706ab4ee4f2729
Anil Kumar <102414580+anil270@users.noreply.github.com>
2022-09-02 23:41:11 +0530

    Add files via upload

---
+++ lambda.py
@@ -0,0 +1,5 @@
+#Write a Python program to create a lambda function that adds 25 to a given number passed in as an argument.
+
+n = int(input("Enter the number :"))
+add = lambda x:x+25
+print(add(n))
